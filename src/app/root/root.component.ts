0df93c69d924da6f2c2cc23f35b2228b75fc93f2
nitishmakam <nitishmakam@gmail.com>
2018-11-07 21:23:00 +0530

    Preloader for username validity check

--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -17,17 +17,24 @@ export class RootComponent implements OnInit {
   private usernameValidShow: boolean;
   private signin: SignInCredentials;
   private signup: SignUpCredentials;
+  private showLoader: boolean;
   constructor(private router: Router, private userService: UserService) {
     this.sign = true;
     this.signin = new SignInCredentials();
     this.signup = new SignUpCredentials();
     this.usernameValid = false;
     this.usernameValidShow = false;
+    this.showLoader = true;
   }
 
   ngOnInit() {
   }
-
+  signTrue() {
+    this.sign = true;
+  }
+  signFalse() {
+    this.sign = false;
+  }
   signIn() {
     console.log(this.signin);
     this.userService.signIn(this.signin);
@@ -40,10 +47,20 @@ export class RootComponent implements OnInit {
     this.userService.signUp(this.signup);
   }
   validateUsername() {
+    this.showLoader = true;
     if (this.signup.username != null && this.signup.username.length !== 0) {
-      this.usernameValidShow = true;
       this.userService.validateUsername(this.signup.username)
-        .subscribe(x => this.usernameValid = true, err => this.usernameValid = false);
+        .subscribe(
+          x => {
+            this.usernameValid = true;
+            this.usernameValidShow = true;
+            this.showLoader = false;
+          },
+          err => {
+            this.usernameValid = false;
+            this.usernameValidShow = true;
+            this.showLoader = false;
+          });
     }
   }
 }
